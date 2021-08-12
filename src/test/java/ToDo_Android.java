fe29b4327174e85723a35f19d380153b051c797c
marcusalten <marcus.cerezuela@alten.es>
2021-08-12 13:15:58 +0200

    Added object dataprovider to Android Test

--- src/test/java/ToDo_Android.java
+++ src/test/java/ToDo_Android.java
@@ -1,33 +1,32 @@
-import org.json.simple.parser.ParseException;
+import dataobjects.tasks.TasksDatum;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pageobjects.CreateTaskPage;
 import pageobjects.TasksListPage;
-import utils.JsonReader;
+import utils.DataObjects;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Iterator;
+
 
 public class ToDo_Android extends TestBase {
 
     CreateTaskPage createTaskPage ;
     TasksListPage tasksListPage;
 
-    @DataProvider(name = "tasks data")
-    public Object[][] passData() throws IOException, ParseException {
-        return JsonReader.getJSONdata
-                (System.getProperty("user.dir") + "/data/TasksData.json"
-                        , "Tasks Data", 2);
+    @DataProvider(name = "tasksObject")
+    public Iterator<Object[]> passData(){
+        return DataObjects.getTasksIterator();
     }
 
-    @Test(dataProvider = "tasks data")
-    public void test_add_task(String taskName, String TaskDesc) throws MalformedURLException {
+    @Test(dataProvider = "tasksObject")
+    public void test_add_task(TasksDatum task) throws MalformedURLException {
         Android_setUp();
         tasksListPage = new TasksListPage(driver);
         createTaskPage = new CreateTaskPage(driver);
         tasksListPage.clickAddTaskBtn();
-        createTaskPage.enterTaskName(taskName);
-        createTaskPage.enterTaskDesc(TaskDesc);
+        createTaskPage.enterTaskName(task.getTaskName());
+        createTaskPage.enterTaskDesc(task.getTaskDesc());
         driver.hideKeyboard();
         createTaskPage.clickSaveBtn();
         createTaskPage.getScreenShot();
