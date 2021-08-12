33032e25499089611d7ea17ed12cd6a474d960cb
marcusalten <marcus.cerezuela@alten.es>
2021-08-12 11:17:25 +0200

    Added DataDriven Example

--- src/main/java/pageobjects/PageBase.java
+++ src/main/java/pageobjects/PageBase.java
@@ -1,9 +1,20 @@
+package pageobjects;
+
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Timer;
+
+import org.apache.commons.io.FileUtils;
 
 public class PageBase {
 
@@ -38,6 +49,15 @@ public class PageBase {
     public String getAttribute(MobileElement element, String attribute) {
         waitForVisibility(element);
         return element.getAttribute(attribute);
+    }
+
+    public void getScreenShot(){
+        File file  = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file, new File( new Date().getTime()+"_Screenshot.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/test/java/ToDo_Android.java
+++ src/test/java/ToDo_Android.java
@@ -1,5 +1,11 @@
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import pageobjects.CreateTaskPage;
+import pageobjects.TasksListPage;
+import utils.JsonReader;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 
 public class ToDo_Android extends TestBase {
@@ -7,16 +13,24 @@ public class ToDo_Android extends TestBase {
     CreateTaskPage createTaskPage ;
     TasksListPage tasksListPage;
 
-    @Test
-    public void test_add_task() throws MalformedURLException {
+    @DataProvider(name = "tasks data")
+    public Object[][] passData() throws IOException, ParseException {
+        return JsonReader.getJSONdata
+                (System.getProperty("user.dir") + "/data/TasksData.json"
+                        , "Tasks Data", 2);
+    }
+
+    @Test(dataProvider = "tasks data")
+    public void test_add_task(String taskName, String TaskDesc) throws MalformedURLException {
         Android_setUp();
         tasksListPage = new TasksListPage(driver);
         createTaskPage = new CreateTaskPage(driver);
         tasksListPage.clickAddTaskBtn();
-        createTaskPage.enterTaskName("Finish Appium Course");
-        createTaskPage.enterTaskDesc("Finishing my course ASAP");
+        createTaskPage.enterTaskName(taskName);
+        createTaskPage.enterTaskDesc(TaskDesc);
         driver.hideKeyboard();
         createTaskPage.clickSaveBtn();
+        createTaskPage.getScreenShot();
         tearDown();
     }
 }
